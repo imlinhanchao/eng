52268b1f00e750cfa8a3251b8acdca03da96f0a0
Hancel.Lin <imlinhanchao@gmail.com>
2018-05-30 22:54:34 +0800

    添加菜单和登录modal。

--- frontend/views/index.vue
+++ frontend/views/index.vue
@@ -37,6 +37,7 @@
 }
 .layout-sidebar {
     position: fixed;
+    left: 0;
     height: 100%;
     z-index: 999;
 }
@@ -101,9 +102,44 @@
     bottom: 1.5em;
     right: 3em;
 }
+.login-footer {
+    text-align: center
+}
 </style>
 <template>
     <Layout class="layout">
+        <Sider :class="'layout-sidebar'" default-collapsed breakpoint="xl" collapsible :collapsed-width="0" v-model="isCollapsed">
+            <Header class="layout-user" v-if="isLogin">
+                <Icon type="person"></Icon>
+                <span>{{name}}</span>
+            </Header>
+            <Menu active-name="1-1" theme="dark" width="auto" :class="menuitemClasses">
+                <MenuItem name="1-1" v-if="isLogin">
+                    <router-link to="/"><Icon type="ios-search-strong"></Icon>
+                    <span>查询单词</span></router-link>
+                </MenuItem>
+                <MenuItem name="1-2" v-if="isLogin">
+                    <router-link to="/notes"><Icon type="document-text"></Icon>
+                    <span>我的笔记</span></router-link>
+                </MenuItem>
+                <MenuItem name="1-3" v-if="isLogin">
+                    <router-link to="/star"><Icon type="android-star"></Icon>
+                    <span>收藏的笔记</span></router-link>
+                </MenuItem>
+                <MenuItem name="1-4" v-if="isLogin">
+                    <router-link to="/logout"><Icon type="log-out"></Icon>
+                    <span>退出</span></router-link>
+                </MenuItem>
+                <MenuItem name="1-5" v-if="!isLogin">
+                    <span @click="loginModel = true"><Icon type="log-in"></Icon>
+                    <span>登录</span></span>
+                </MenuItem>
+            </Menu>
+            <Footer class="layout-footer">
+                <p>{{ (new Date()).getFullYear() }} &copy; <a hrep="https://github.com/imlinhanchao/eng" target="_blank">Hancel Lin</a></p>
+            </Footer>
+            <div slot="trigger"></div>
+        </Sider>
         <Layout class="layout-main" :class="collapsed">
             <Header class="layout-header">
                 <div class="layout-logo">
@@ -116,13 +152,47 @@
                 </Layout>
             </Content>
         </Layout>
+        <Modal v-model="loginModel" title="登录" width="300">
+            <Form ref="loginForm" :model="login" :rules="ruleValidate" class="layout-form">
+                <FormItem prop="user">
+                    <Input type="text" v-model="login.username" placeholder="用户名" @keyup.13="document.getElementById('password').focus()">
+                        <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                </FormItem>
+                <FormItem prop="password">
+                    <Input id="password" :type="passwdType" v-model="login.passwd" placeholder="密码" @keyup.13="loginSubmit('loginForm')">
+                        <Icon type="ios-locked-outline" slot="prepend"></Icon>
+                        <Button slot="append" :icon="showIcon" @click="isPasswdShow=!isPasswdShow" style="box-shadow:none;" :loading="login_loading"></Button>
+                    </Input>
+                </FormItem>
+            </Form>
+            <div slot="footer" class="login-footer">
+                <Button type="primary" @click="loginSubmit('loginForm')" :loading="login_loading">登录</Button>
+            </div>
+        </Modal>
     </Layout>
 </template>
 <script>
     export default {
         data () {
             return {
-                isCollapsed: false
+                isCollapsed: false,
+                loginUser: null,
+                loginModel: false,
+                login: {
+                    username: '',
+                    passwd: ''
+                },
+                isPasswdShow: false,
+                ruleValidate: {
+                    username: [
+                        { required: true, message: '请输入用户名。', trigger: 'blur' }
+                    ],
+                    passwd: [
+                        { required: true, message: '请输入密码。', trigger: 'blur' },
+                    ]
+                },
+                login_loading: false
             };
         },
         computed: {
@@ -133,17 +203,29 @@
                 ]
             },
             name () {
-                return this.$root.loginUser ? this.$root.loginUser.name : '';
+                return this.loginUser ? this.loginUser.name : '';
+            },
+            isLogin () {
+                return !!this.loginUser;
             },
             collapsed() {
                 return [
                     this.isCollapsed ? "collapsed" : ""
                 ]
+            },
+            showIcon() {
+                return this.isPasswdShow ? 'eye-disabled' : 'eye'
+            },
+            passwdType () {
+                return this.isPasswdShow ? 'text' : 'password'
             }
         },
         methods: {
             newMeeting() {
                 this.$router.push(`/new`);
+            },
+            loginSubmit() {
+
             }
         }
     }
